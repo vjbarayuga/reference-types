7cdaec0df97d3101c3e2447603cad586acbd076e
vjdbarayuga <vjdbarayuga@gmail.com>
2024-10-10 08:25:53 +0800

    First Commit

---
+++ app.js
@@ -0,0 +1,46 @@
+//NUMBER
+//STRING
+//BOOLEAN
+//NULL/UNDEFINED
+//BIGINT
+//SYMBOL
+
+// let a = 10;
+// let b = a;
+
+// b = 20;
+
+// console.log(a);
+// console.log(b);
+
+
+//REFERENCE TYPES: STORE A REFERENCE(MEMORY ADDRESS) TO THE LOCATION WHERE THE OBJECT OR ARRAY IS STORED.
+//PRIMITIVE TYPES: STORE THE ACTUAL VALUES DIRECTLY IN THE VARIABLE.
+
+// let num1 = 42;
+// let num2 = num1;
+
+// num2 = 100;
+// console.log(num1);
+// console.log(num2);
+
+
+//REFERENCE TYPE EXAMPLE(OBJECTS)
+
+// let obj1 = { name: "Alice" };
+// let obj2 = obj1;
+
+// obj2.name = "Bob";
+
+// console.log(obj1.name);
+// console.log(obj2.name);
+
+
+//MORE EXAMPLES IN REFERENCE TYPE
+// let x = [1, 2, 3];
+// let y = x;
+// y.push(4);
+// console.log(x);
+
+const words = ["hello", "world", "welcome!"];
+console.log(words[0]);
